65307ca0412918b2df3805435b06948de78a72f0
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-27 23:49:15 +0600

    Home component is created

--- HouseRenting-UI/src/app/app.module.ts
+++ HouseRenting-UI/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
