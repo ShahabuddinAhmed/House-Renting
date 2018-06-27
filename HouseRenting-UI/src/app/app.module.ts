4b858329a3d30cd125e2701533f2a0ce09a99c1e
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-28 01:01:38 +0600

    Navbar component is created

--- HouseRenting-UI/src/app/app.module.ts
+++ HouseRenting-UI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
