7616161eacb2ebec4c6f4844178f7d6dc9c04251
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-28 23:37:04 +0600

    home component and path is added

--- HouseRenting-UI/src/app/app-routing.module.ts
+++ HouseRenting-UI/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
