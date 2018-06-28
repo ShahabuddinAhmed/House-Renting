b9b9b36d1c123680d23b991169587be8bbcd8e59
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-28 22:35:44 +0600

    Admin Panel is created

--- HouseRenting-UI/src/app/app.module.ts
+++ HouseRenting-UI/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
