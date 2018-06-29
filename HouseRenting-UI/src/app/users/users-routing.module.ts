97a80072a9d56bae2ab017437aee4820411b2d4a
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-30 00:11:46 +0600

    some route is added

--- HouseRenting-UI/src/app/users/users-routing.module.ts
+++ HouseRenting-UI/src/app/users/users-routing.module.ts
@@ -1,9 +1,23 @@
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const routes: Routes = [
+  { path: "register", component: SignupComponent },
+  { path: "login", component: SigninComponent },
+  { path: "profile", component: ViewProfileComponent }
+]
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
