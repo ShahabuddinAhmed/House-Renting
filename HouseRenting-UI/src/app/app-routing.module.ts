9f2e9a151cd5957753268d82770467c67cc4f9c0
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-30 00:12:24 +0600

    home route is added

--- HouseRenting-UI/src/app/app-routing.module.ts
+++ HouseRenting-UI/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent}
+  { path: "", component: HomeComponent }
 ]
 
 @NgModule({
