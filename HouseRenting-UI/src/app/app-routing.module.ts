2fd3c6d4e74620b097beabdfc061d6f3b75973f3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-06-29 23:39:38 +0600

    CommonModule is imported

--- HouseRenting-UI/src/app/app-routing.module.ts
+++ HouseRenting-UI/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forRoot(routes)
   ],
   exports: [
