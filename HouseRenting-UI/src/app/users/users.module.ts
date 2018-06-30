6f9c8e72184a63a3cced152d4004b068e75b16f0
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-01 00:10:06 +0600

    angular materials is imported

--- HouseRenting-UI/src/app/users/users.module.ts
+++ HouseRenting-UI/src/app/users/users.module.ts
@@ -1,12 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatRippleModule, MatTooltipModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatSidenavModule} from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     SignupComponent,
