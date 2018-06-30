f4d30d262ee9480d44ce9847be37ba7e43adbf34
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-01 01:57:32 +0600

    login content is added

--- HouseRenting-UI/src/app/users/signin/signin.component.ts
+++ HouseRenting-UI/src/app/users/signin/signin.component.ts
@@ -1,4 +1,6 @@
+import { Login } from './../models/login';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  public loginForm: FormGroup;
+  public email: FormControl;
+  public password: FormControl;
+  login: Login;
 
+  private createForm(): void {
+    this.loginForm = new FormGroup( {
+      email: this.email,
+      password: this.password
+    });
+  }
+  private CreateFormControls(): void {
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]);
+    this.password = new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(100)
+    ]);
+  }
   constructor() { }
 
   ngOnInit() {
+    this.CreateFormControls();
+    this.createForm();
+  }
+
+  onSubmit() {
+    this.login = new Login();
+    this.login.userEmail = this.email.value;
+    this.login.userPassword = this.password.value;
+    console.log(this.login.userEmail);
+    console.log(this.login.userPassword);
   }
 
 }
