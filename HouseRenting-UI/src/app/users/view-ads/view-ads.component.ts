ef7f960111cc9ab9d2143c6ebf8e63d6c631fcdd
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-02 00:03:13 +0600

    user view-ads component is created

---
+++ HouseRenting-UI/src/app/users/view-ads/view-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ads',
+  templateUrl: './view-ads.component.html',
+  styleUrls: ['./view-ads.component.css']
+})
+export class ViewAdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
