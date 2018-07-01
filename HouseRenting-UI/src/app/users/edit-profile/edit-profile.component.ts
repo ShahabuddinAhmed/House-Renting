17895997c11adb88e7697df418948b433744a1bf
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-01 23:22:24 +0600

    edit-profile content is added

--- HouseRenting-UI/src/app/users/edit-profile/edit-profile.component.ts
+++ HouseRenting-UI/src/app/users/edit-profile/edit-profile.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Register } from './../models/register';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
+  public Register: FormGroup;
+  public name: FormControl;
+  public email: FormControl;
+  public password: FormControl;
+  public confirmPassword: FormControl;
+  register: Register;
+
+  private createFormGroup(): void {
+    this.Register = new  FormGroup( {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    });
+  }
+
+  private createFormControls(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30)
+    ]);
+    this.confirmPassword = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30)
+    ]);
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createFormGroup();
+  }
+
+  onSubmit() {
+    this.register = new Register();
+    this.register.userName = this.name.value;
+    this.register.userEmail = this.email.value;
+    this.register.userPassword = this.password.value;
+    this.register.userConfirmPassword = this.confirmPassword.value;
+    console.log(this.register.userName);
+    console.log(this.register.userEmail);
+    console.log(this.register.userPassword);
+    console.log(this.register.userConfirmPassword);
   }
 
 }
