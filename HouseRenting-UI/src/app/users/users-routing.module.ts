3b48d4aa25b73bb856df3c3cca82da6013da0fcf
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-01 23:19:46 +0600

    some route is imported

--- HouseRenting-UI/src/app/users/users-routing.module.ts
+++ HouseRenting-UI/src/app/users/users-routing.module.ts
@@ -1,14 +1,19 @@
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: "register", component: SignupComponent },
   { path: "login", component: SigninComponent },
-  { path: "profile", component: ViewProfileComponent }
+  { path: "profile", component: ViewProfileComponent },
+  { path: "user", component: UserComponent },
+  { path: "view", component: ViewProfileComponent },
+  { path: "edit", component: EditProfileComponent }
 ]
 
 @NgModule({
