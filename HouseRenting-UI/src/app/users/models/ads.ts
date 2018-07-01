24607048c2d606620fc2dfecd86641a14e4578d9
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-01 13:08:15 +0600

    user ads model is added

---
+++ HouseRenting-UI/src/app/users/models/ads.ts
@@ -0,0 +1,12 @@
+export class House {
+    userTitle: string;
+    userDescription: string;
+    userTaka: string;
+    userAddress: string;
+    userPhon: string;
+    userArea: string;
+    userBeadRoom: string;
+    userKitchen: string;
+    userWashRoom: string;
+    userID: string;
+}
