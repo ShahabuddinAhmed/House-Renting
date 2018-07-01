0f81f822008fce5f2ec430ee43866a489276aaea
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-07-02 00:02:49 +0600

    user edit-ads component is created

---
+++ HouseRenting-UI/src/app/users/edit-ads/edit-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-ads',
+  templateUrl: './edit-ads.component.html',
+  styleUrls: ['./edit-ads.component.css']
+})
+export class EditAdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
