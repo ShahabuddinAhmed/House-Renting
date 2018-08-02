32e5edf7924fa5c1a60461cc400ab514948dd3f0
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-08-02 22:21:40 +0600

    some file is imported

--- HouseRenting-UI/src/app/app.module.ts
+++ HouseRenting-UI/src/app/app.module.ts
@@ -5,25 +5,25 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AdminComponent } from './admin/admin.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AdminRoutingModule } from './admin/admin-routing.module';
 import { UsersRoutingModule } from './users/users-routing.module';
 import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomeComponent,
-    NavbarComponent,
-    AdminComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AdminRoutingModule,
     UsersRoutingModule,
-    UsersModule
+    UsersModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
