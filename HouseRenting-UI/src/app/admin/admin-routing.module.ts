1996101dcd28bfce7608a6038be7985903f6c9de
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-08-06 23:01:55 +0600

    some admin module is imported

--- HouseRenting-UI/src/app/admin/admin-routing.module.ts
+++ HouseRenting-UI/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,19 @@
+import { AdminComponent } from './admin.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const routes: Routes = [
+  { path: "admin", component: AdminComponent }
+]
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   declarations: []
 })
