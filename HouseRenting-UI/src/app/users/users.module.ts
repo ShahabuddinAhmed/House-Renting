2a13c6062ea9df28cd509301541476a5dda0b09c
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-30 21:36:07 +0600

    user module is updated

--- HouseRenting-UI/src/app/users/users.module.ts
+++ HouseRenting-UI/src/app/users/users.module.ts
@@ -2,9 +2,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatRippleModule, MatTooltipModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule, MatSidenavModule} from '@angular/material';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatRippleModule,
+  MatTooltipModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatSidenavModule
+} from '@angular/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UserComponent } from './user/user.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
@@ -28,8 +41,10 @@ import { ViewAdsComponent } from './view-ads/view-ads.component';
     MatCardModule,
     MatSidenavModule,
     MatTooltipModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   declarations: [
     SignupComponent,
     SigninComponent,
