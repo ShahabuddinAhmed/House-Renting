8f628954d2148102da8bd047d3b30b652e6bb6bd
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-31 10:10:56 +0600

    home component is updated

--- HouseRenting-UI/src/app/home/home.component.ts
+++ HouseRenting-UI/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Search } from '../model/search';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public Search: FormGroup;
+  public district: FormControl;
+  public location: FormControl;
+  public minMoney: FormControl;
+  public maxMoney: FormControl;
+  public minArea: FormControl;
+  public maxArea: FormControl;
+  public roomNumber: FormControl;
+  public search: Search;
+
+  private createForm(): void {
+    this.Search = new FormGroup( {
+      district: this.district,
+      location: this.location,
+      minMoney: this.minMoney,
+      maxMoney: this.maxMoney,
+      minArea: this.minArea,
+      maxArea: this.maxArea,
+      roomNumber: this.roomNumber,
+
+    });
+  }
+
+  private CreateFormControls(): void {
+    this.district = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.location = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.minMoney = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.maxMoney = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.minArea = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.maxArea = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.roomNumber = new FormControl('', [
+      Validators.required
+    ]);
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.CreateFormControls();
+    this.createForm();
+  }
+
+  onSubmit() {
+
   }
 
 }
