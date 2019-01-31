e754d99909ac282ad1305bdbe9f7d1bd39180cc2
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-01-31 10:16:38 +0600

    admin location service is added

---
+++ HouseRenting-UI/src/app/admin/service/location.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Division } from '../models/division';
+import { Location } from '../models/location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  _addLocation(_location: Location) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post('http://localhost:3000/admin/division', _location, httpOptions);
+  }
+
+  _getLocation(): Observable<Location[]> {
+    return this.http.get<Location[]>('http://localhost:3000/admin/minarea');
+  }
+
+  _deleteLocation(id: string) {
+    return this.http.delete(`http://localhost:3000/admin/delete/${id}`);
+  }
+
+  _getDivision(): Observable<Division[]> {
+    return this.http.get<Division[]>('http://localhost:3000/admin/minarea');
+  }
+}
