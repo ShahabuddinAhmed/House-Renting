3f945785a1e9ed21e5be35ff9259c79e04ca50aa
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-07 17:24:40 +0600

    user addimage logic is updated

--- HouseRenting-UI/src/app/users/addimage/addimage.component.ts
+++ HouseRenting-UI/src/app/users/addimage/addimage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ImageService } from '../service/image.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addimage',
@@ -13,9 +14,22 @@ export class AddimageComponent implements OnInit {
   public adsID: string;
   public formData: any;
   public storeFile: Array<File>;
+  public name1: FormControl;
+  public name2: FormControl;
+  public customFile: File = null;
+
+  private CreateFormControls(): void {
+    this.name1 = new FormControl('', [
+      Validators.required
+    ]);
+    this.name2 = new FormControl('', [
+      Validators.required
+    ]);
+  }
   constructor(private _imageService: ImageService, private router: Router) { }
 
   ngOnInit() {
+    this.CreateFormControls();
   }
 
 //   upload() {
@@ -32,12 +46,31 @@ export class AddimageComponent implements OnInit {
 //     //     .subscribe(files => console.log('files', files));
 // }
 
-fileChangeEvent(fileInput: any) {
+fileChangeEvent1(fileInput: any) {
+  this.customFile = fileInput.target.files[0];
+  // this.product.photo = fileInput.target.files[0]['name'];
+  console.log(this.customFile);
+}
+
+fileChangeEvent2(fileInput: any) {
     this.filesToUpload = <Array<File>>fileInput.target.files;
     // this.product.photo = fileInput.target.files[0]['name'];
 }
 
-upload() {
+upload1() {
+  const userID = '5c49cbb4a2cb7302e022d4cc';
+  const fd = new FormData();
+    fd.append('picture', this.customFile, this.customFile.name);
+    fd.append('userId', userID);
+    this._imageService._uploadCoverImage(this.customFile, userID).subscribe(data => {
+      // console.log(data["message"]);
+     console.log(data);
+    }, err => {
+      console.log(err);
+    });
+}
+
+upload2() {
   this.adsID = '5c4fef8d6c190a4090742458';
   this.formData = new FormData();
   for (let i = 0; i < this.filesToUpload.length; i++) {
