768723dfdbf2c11c5b4b1c51469094d1b2563d30
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-07 17:15:34 +0600

    ads model is updated

--- HouseRenting-UI/src/app/users/models/ads.ts
+++ HouseRenting-UI/src/app/users/models/ads.ts
@@ -1,12 +1,17 @@
 export class House {
-    userTitle: string;
-    userDescription: string;
-    userTaka: string;
-    userAddress: string;
-    userPhon: string;
-    userArea: string;
-    userBeadRoom: string;
-    userKitchen: string;
-    userWashRoom: string;
-    userID: string;
+  _id: string;
+  title: string;
+  description: string;
+  taka: number;
+  address: string;
+  phon: string;
+  area: number;
+  bedRoom: number;
+  kitchen: number;
+  washRoom: number;
+  division: string;
+  location: string;
+  coverImage: string;
+  _date: Date;
+  userID: string;
 }
