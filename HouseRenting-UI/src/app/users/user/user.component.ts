872e634d6d606ba55741a2e798cc2e8022149d5b
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-08 23:57:37 +0600

    user component logic is updated

--- HouseRenting-UI/src/app/users/user/user.component.ts
+++ HouseRenting-UI/src/app/users/user/user.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-user',
@@ -13,11 +14,69 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class UserComponent implements OnInit {
   jwtHelper = new JwtHelperService();
   public userID: string;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private _userService: UserService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     // this.getUserID();
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      // max-width 800
+      {
+          breakpoint: 800,
+          width: '100%',
+          height: '600px',
+          imagePercent: 80,
+          thumbnailsPercent: 20,
+          thumbnailsMargin: 20,
+          thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+    },
+    {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+      },
+      {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+      },
+      {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+      },
+      {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+      },
+      {
+        small: 'assets/images/1.jpg',
+        medium: 'assets/images/1.jpg',
+        big: 'assets/images/1.jpg'
+      }
+    ];
   }
 
   onSubmit() {
