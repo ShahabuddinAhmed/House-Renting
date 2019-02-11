74f225878501fabca32013b2d1c5f70105c81c1d
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-11 17:37:38 +0600

    app module is updated

--- HouseRenting-UI/src/app/app.module.ts
+++ HouseRenting-UI/src/app/app.module.ts
@@ -11,20 +11,60 @@ import { AdminRoutingModule } from './admin/admin-routing.module';
 import { UsersRoutingModule } from './users/users-routing.module';
 import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
+import {
+  MatSelectModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatRippleModule,
+  MatMenuModule,
+  MatListModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatTooltipModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgxGalleryModule } from 'ngx-gallery';
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxGalleryModule,
     AdminRoutingModule,
     UsersRoutingModule,
     UsersModule,
     AdminModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatSelectModule,
+    HttpClientModule,
+    RouterModule,
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
