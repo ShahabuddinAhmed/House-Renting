2ff3a74caa7e411f0d419d792c1dd321618731ea
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-15 09:35:22 +0600

    login service is added

--- HouseRenting-UI/src/app/admin/admin-login/admin-login.component.ts
+++ HouseRenting-UI/src/app/admin/admin-login/admin-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AdminLogin } from '../models/admin-login';
+import { Router } from '@angular/router';
+import { AccountService } from '../service/account.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  public loginForm: FormGroup;
+  public email: FormControl;
+  public password: FormControl;
+  login: AdminLogin;
+
+  private createForm(): void {
+    this.loginForm = new FormGroup( {
+      email: this.email,
+      password: this.password
+    });
+  }
+  private CreateFormControls(): void {
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(60),
+      Validators.email
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(100)
+    ]);
+  }
+  constructor(private _userService: AccountService, private router: Router) { }
 
   ngOnInit() {
+    this.CreateFormControls();
+    this.createForm();
+  }
+
+  onSubmit() {
+    this.login = new AdminLogin();
+    this.login.adminEmail = this.email.value;
+    this.login.adminPassword = this.password.value;
+    this._userService._adminLogin(this.login)
+    .subscribe(data => {
+      console.log(data);
+      this._userService.setToken(data);
+      this.router.navigate(['admin']);
+      },
+      err => {
+        console.log(err);
+      });
   }
 
 }
