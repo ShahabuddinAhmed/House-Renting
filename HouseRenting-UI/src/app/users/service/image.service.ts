1d8c1b1b5d7b595dada3f79d0caa27cc38c81fd1
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-02-20 08:19:31 +0600

    function name is changed

--- HouseRenting-UI/src/app/users/service/image.service.ts
+++ HouseRenting-UI/src/app/users/service/image.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { HouseImage } from '../models/houseImage';
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +26,9 @@ export class ImageService {
       })
     };
     return this.http.post(`http://localhost:3000/houseimg/create/`, fd, httpOptions);
+  }
+
+  getImage(id: string): Observable<HouseImage[]> {
+    return this.http.get<HouseImage[]>(`http://localhost:3000/houseimg/${id}`);
   }
 }
