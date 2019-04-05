12dde799e56eb25a03656f1839e312e4d060d380
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-04-06 01:34:27 +0600

    admin service is added

---
+++ HouseRenting-UI/src/app/admin/service/admin.service.ts
@@ -0,0 +1,11 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+}
