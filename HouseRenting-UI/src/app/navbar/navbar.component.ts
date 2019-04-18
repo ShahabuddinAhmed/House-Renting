ca5959f159bd7db260f603396ad915361683caa3
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-04-19 00:44:04 +0600

    navbar is ui and logic is added

--- HouseRenting-UI/src/app/navbar/navbar.component.ts
+++ HouseRenting-UI/src/app/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
+import { AccountService } from './../admin/service/account.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { CommonService } from './../auth/common.service';
+import { AuthService } from './../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../users/user.service';
+import { RoleGuardService } from '../auth/role-guard.service';
+import { UserGuardService } from '../auth/user-guard.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +14,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  jwtHelper = new JwtHelperService();
+  public userID: string;
+  public adminID: string;
+  public userName: string;
+  public adminName: string;
+
+  constructor(
+    private _userService: UserService,
+    public _authService: AuthService,
+    public _rollGuardService: RoleGuardService,
+    public _userGuardService: UserGuardService,
+    private _accountService: AccountService,
+    public _commonService: CommonService
+    ) { }
 
   ngOnInit() {
+    this.getUserID();
+  }
+
+  public getUserID() {
+    const token = localStorage.getItem('token');
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+    this.userID = tokenPayload.userID;
+    return this.userID;
+  }
+
+  public getAdminID() {
+    const token = localStorage.getItem('token');
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+    this.adminID = tokenPayload.adminID;
+    return this.adminID;
+  }
+
+  getName() {
+    const token = localStorage.getItem('token');
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+    return this.userName = tokenPayload.userName;
+  }
+
+  getAdminName() {
+    const token = localStorage.getItem('token');
+    const tokenPayload = this.jwtHelper.decodeToken(token);
+    return this.adminName = tokenPayload.adminName;
+  }
+
+  loggedOut() {
+    this._userService.loggedOut();
+  }
+
+  adminLoggedOut() {
+    this._accountService.loggedOut();
   }
 
 }
